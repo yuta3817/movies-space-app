85452034f91eb219311aa664a89a509a71190c54
yuta3817 <yuta2213817@gmail.com>
2020-10-02 21:07:44 +0900

    Asynchronous communication

--- app/assets/javascripts/autoReload.js
+++ app/assets/javascripts/autoReload.js
@@ -40,28 +40,27 @@ $(function(){
     };
   }
 
-  $('.Form').on('submit', function(e){
-    e.preventDefault();
-    let formData = new FormData(this);
-    let url = $(this).attr('action');
+  let reloadMessages = function() {
+    let last_message_id = $('.MessageBox:last').data("message-id") || 0;
     $.ajax({
-      url: url,
-      type: "POST",
-      data: formData,
+      url: "api/messages",
+      type: 'get',
       dataType: 'json',
-      processData: false,
-      contentType: false
+      data: {id: last_message_id}
     })
-    .done(function(data){
-      let html = buildHTML(data);
-      $('.MessageField').append(html); 
-      $('.MessageField').animate({ scrollTop: $('.MessageField')[0].scrollHeight});     
-      $('form')[0].reset();
-      $('.Form__submit').prop("disabled", false);
+    .done(function(messages) {
+      if (messages.length !== 0) {
+        let insertHTML = '';
+        $.each(messages, function(i, message) {
+          insertHTML += buildHTML(message)
+        });
+        $('.MessageField').append(insertHTML);
+        $('.MessageField').animate({ scrollTop: $('.MessageField')[0].scrollHeight});
+      }
     })
     .fail(function() {
-      alert("メッセージ送信に失敗しました");
-      $('.Form__submit').prop("disabled", false);
+      alert('error');
     });
-  });
+  };
+  setInterval(reloadMessages, 7000);
 });
